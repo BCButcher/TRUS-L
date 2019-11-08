fa7802298f2ed3725b23e60e8b856b3a6351099a
Chineng Pong <chinengpong@hotmail.com>
2019-11-08 13:41:14 -0500

    fixing js file

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,19 +1,6 @@
-const express = require('express');
-
-
-const app =express();
+$('#sellBtn')
 
 
 
-app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/api', function (req,res){
-
-    
-}); 
-
-
-
-const PORT = process.env.PORT || 3000;
-
-app.listen( PORT, () =>  console.log(`server started  on port ${PORT}`))
+$.get()
