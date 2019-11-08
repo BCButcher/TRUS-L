4b8c29d46c6cda90e796da7950a2047d557bf9ea
ruthtech <leeruths@gmail.com>
2019-11-08 09:59:41 -0500

    Update schema, seed, sample queries as per feedback and tests. Start option mapper to translate between checkbox and database.

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS TRUSL;
 CREATE DATABASE TRUSL;
 use TRUSL;
 
-CREATE TABLE agent (
+CREATE TABLE agents (
    license INTEGER NOT NULL PRIMARY KEY,
    first_name VARCHAR(32),
    last_name VARCHAR(32),
@@ -11,23 +11,22 @@ CREATE TABLE agent (
    web_site VARCHAR(2083)
 );
 
-CREATE TABLE consumer (
+CREATE TABLE consumers (
 	id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     display_name VARCHAR(32),
     first_name VARCHAR(32),
     last_name VARCHAR(32),
-    email VARCHAR(32),
-	phone VARCHAR(15)
+    email VARCHAR(32)
 );
 
-CREATE TABLE listing (
+CREATE TABLE listings (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     poster_id INTEGER,
     area VARCHAR(32),
     listing_status ENUM("Active", "Booked", "Closed"),
     money INTEGER,
     transaction_type VARCHAR(4),
-    FOREIGN KEY (poster_id) REFERENCES consumer(id)
+    FOREIGN KEY (poster_id) REFERENCES consumers(id)
 );
 
 CREATE TABLE bids (
@@ -37,8 +36,8 @@ CREATE TABLE bids (
    bid_status ENUM("Active", "Booked", "Closed"),
    services VARCHAR(8),
    message VARCHAR(500),    
-   FOREIGN KEY (agent_id) REFERENCES agent(license),
-   FOREIGN KEY (listing_id) REFERENCES listing(id)
+   FOREIGN KEY (agent_id) REFERENCES agents(license),
+   FOREIGN KEY (listing_id) REFERENCES listings(id)
 );
 
 -- ENUM("Rent", "Buy", "Sell", "Rent out"),
--- db/seed.sql
+++ db/seed.sql
@@ -1,22 +1,22 @@
 use TRUSL;
 
-INSERT INTO agent (license, first_name, last_name, email, phone, web_site) VALUES (123456789, "Abby", "Banksy", "abbybanksy@broker.ca", "416-123-4567", "https://www.abbybanksy.com");
-INSERT INTO agent (license, first_name, last_name, email, phone, web_site) VALUES (234567890, "Charles", "Daly", "charlesdaly@broker.ca", "416-234-5678", "https://www.charlesdaly.com");
-INSERT INTO agent (license, first_name, last_name, email, phone, web_site) VALUES (345678901, "Kanchani", "Franks", "kanchanifranks@broker.ca", "647-123-4567", "https://www.kanchanifranks.com");
-INSERT INTO agent (license, first_name, last_name, email, phone, web_site) VALUES (456789012, "Hashim", "Siyam", "hashimsiyam@broker.ca", "647-456-7890", "https://www.hashimsiyam.com");
-INSERT INTO agent (license, first_name, last_name, email, phone, web_site) VALUES (567890123, "Rupam", "Smith", "rupamsmith@broker.ca", "416-789-1234", "https://www.rupamsmith.com");
+INSERT INTO agents (license, first_name, last_name, email, phone, web_site) VALUES (123456789, "Abby", "Banksy", "abbybanksy@broker.ca", "416-123-4567", "https://www.abbybanksy.com");
+INSERT INTO agents (license, first_name, last_name, email, phone, web_site) VALUES (234567890, "Charles", "Daly", "charlesdaly@broker.ca", "416-234-5678", "https://www.charlesdaly.com");
+INSERT INTO agents (license, first_name, last_name, email, phone, web_site) VALUES (345678901, "Kanchani", "Franks", "kanchanifranks@broker.ca", "647-123-4567", "https://www.kanchanifranks.com");
+INSERT INTO agents (license, first_name, last_name, email, phone, web_site) VALUES (456789012, "Hashim", "Siyam", "hashimsiyam@broker.ca", "647-456-7890", "https://www.hashimsiyam.com");
+INSERT INTO agents (license, first_name, last_name, email, phone, web_site) VALUES (567890123, "Rupam", "Smith", "rupamsmith@broker.ca", "416-789-1234", "https://www.rupamsmith.com");
 
-INSERT INTO consumer (display_name, first_name, last_name, email, phone) VALUES ("Sleepless_in_Toronto", "Annie", "Reed", "anniereed@fake.com", "555-555-5555");
-INSERT INTO consumer (display_name, first_name, last_name, email, phone) VALUES ("Neo", "Thomas A.", "Anderson", "thomasaanderson@fake.com", "555-123-4567");
-INSERT INTO consumer (display_name, first_name, last_name, email, phone) VALUES ("Super", "Louie", "Kritski", "louiekritski@fake.com", "555-555-1234");
+INSERT INTO consumers (display_name, first_name, last_name, email) VALUES ("Sleepless_in_Toronto", "Annie", "Reed", "anniereed@fake.com");
+INSERT INTO consumers (display_name, first_name, last_name, email) VALUES ("Neo", "Thomas A.", "Anderson", "thomasaanderson@fake.com");
+INSERT INTO consumers (display_name, first_name, last_name, email) VALUES ("Super", "Louie", "Kritski", "louiekritski@fake.com");
 
-INSERT INTO listing (poster_id, area, listing_status, money, transaction_type) VALUES (1, "E02", "Active", 1000000, "b");
-INSERT INTO listing (poster_id, area, listing_status, money, transaction_type) VALUES (2, "E01", "Active", 2000, "a");
-INSERT INTO listing (poster_id, area, listing_status, money, transaction_type) VALUES (3, "E11", "Active", 3000, "d");
-INSERT INTO listing (poster_id, area, listing_status, money, transaction_type) VALUES (3, "E11", "Active", 1800, "d");
-INSERT INTO listing (poster_id, area, listing_status, money, transaction_type) VALUES (3, "E12", "Active", 2500, "d");
-INSERT INTO listing (poster_id, area, listing_status, money, transaction_type) VALUES (3, "E06", "Active", 3600, "d");
-INSERT INTO listing (poster_id, area, listing_status, money, transaction_type) VALUES (3, "E14", "Active", 3500, "d");
+INSERT INTO listings (poster_id, area, listing_status, money, transaction_type) VALUES (1, "E02", "Active", 1000000, "b");
+INSERT INTO listings (poster_id, area, listing_status, money, transaction_type) VALUES (2, "E01", "Active", 2000, "a");
+INSERT INTO listings (poster_id, area, listing_status, money, transaction_type) VALUES (3, "E11", "Active", 3000, "d");
+INSERT INTO listings (poster_id, area, listing_status, money, transaction_type) VALUES (3, "E11", "Active", 1800, "d");
+INSERT INTO listings (poster_id, area, listing_status, money, transaction_type) VALUES (3, "E12", "Active", 2500, "d");
+INSERT INTO listings (poster_id, area, listing_status, money, transaction_type) VALUES (3, "E06", "Active", 3600, "d");
+INSERT INTO listings (poster_id, area, listing_status, money, transaction_type) VALUES (3, "E14", "Active", 3500, "d");
 
 INSERT INTO bids (agent_id, listing_id, bid_status, services, message) VALUES (123456789, 1, "Active", "abc", "Hire me because I am in the top 1% of all realtors in the area.");
 INSERT INTO bids (agent_id, listing_id, bid_status, services, message) VALUES (234567890, 1, "Active", "f", "Hire me because I am your mother.");
