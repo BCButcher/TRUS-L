cb8f3b3198f1e1a77f4954b22ee5eb5a6de755fd
ruthtech <leeruths@gmail.com>
2019-11-08 12:28:41 -0500

    Add more routes for static HTML files. Can't test until the HTML files exist.

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -5,6 +5,65 @@ module.exports = function(app) {
 	app.get("/", function(req, res) {
 	    res.sendFile(path.join(__dirname, "/public/index.html"));
 	});
+
+	// Login screen
+	app.get("/login", function(req, res) {
+		res.sendFile(path.join(__dirname, "/public/login.html"));
+	});
+	
+	// User creates a profile
+	app.get("/profile/:type", function(req, res) {
+		if(req.params.type == "agent") {
+			res.sendFile(path.join(__dirname, "/public/profilea.html"));
+		}
+		else {
+			res.sendFile(path.join(__dirname, "/public/profilec.html"));
+		}
+	});
+
+	// User has posted a listing
+	app.get("/confirm", function(req, res) {
+		res.sendFile(path.join(__dirname, "/public/confirm.html"));
+	});
+
+	// List the active bids
+	app.get("/bids", function(req, res) {
+		res.sendFile(path.join(__dirname, "/public/bids.html"));
+	});
+
+	// After the user has rejected a bid, this page asks if they want to tell the
+	// agent why.
+	app.get("/process_bid", function(req, res) {
+		res.sendFile(path.join(__dirname, "/public/process_bid.html"));
+	});
+
+	// View a particular bid. 
+	app.get("/viewbid", function(req, res) {
+		res.sendFile(path.join(__dirname, "/public/agent_bid.html"));
+	});
+
+	// Tell the user that their listing has been posted
+	app.get("/confirm", function(req, res) {
+		res.sendFile(path.join(__dirname, "/public/confirm.html"));
+	});
+
+	// View all active listings
+	app.get("/listings", function(req, res) {
+		res.sendFile(path.join(__dirname, "/public/listings.html"));
+	});
+
+	// Static contact us information (Trusael)
+	app.get("/contact", function(req, res) {
+		res.sendFile(path.join(__dirname, "/public/contact.html"));
+	});
+
+	// Static blurb about Trusael
+	app.get("/about", function(req, res) {
+		res.sendFile(path.join(__dirname, "/public/about.html"));
+	});
+
+
 }
 
 
+ 
