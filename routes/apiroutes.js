01a3d4685e113cbf2e578bb4b5bd1d3a69284c9b
ruthtech <leeruths@gmail.com>
2019-11-09 00:04:40 -0500

    Converting JSON backend to MySQL. Not working yet; checking into branch to save a copy before proceeding. getListings() works.

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,298 +1,279 @@
-let DBAccess = require("../lib/jsonAccess");
+let DBAccess = require('../lib/dbAccess');
 
-let agents = new DBAccess("./db/agents.json");
-let consumers = new DBAccess("./db/consumers.json");
-let bids = new DBAccess("./db/bids.json");
-let listings = new DBAccess("./db/listings.json");
+const dbAccess = new DBAccess();
 
 module.exports = function (app) {
-	// Retrieve all agents
-	app.get("/api/agent", function (req, res) {
-		try {
-			res.status(200);
-			res.send(agents.getRecords());
-		}
-		catch (err) {
-			// Internal error on the server side.
-			console.log(err);
-			res.status(500);
-			res.json(err);
-		}
-		return res;
-	});
+  // Retrieve all agents
+  app.get('/api/agent', function (req, res) {
+    try {
+      res.status(200);
+      res.send(dbAccess.getAgents());
+    } catch (err) {
+      // Internal error on the server side.
+      console.log(err);
+      res.status(500);
+      res.json(err);
+    }
+    return res;
+  });
 
-	// Retrieve all consumers
-	app.get("/api/consumer", function (req, res) {
-		try {
-			res.status(200);
-			res.json(consumers.getRecords());
-		}
-		catch (err) {
-			// Internal error on the server side.
-			console.log(err);
-			res.status(500);
-			res.json(err);
-		}
-		return res;
-	});
+  // Retrieve all consumers
+  app.get('/api/consumer', function (req, res) {
+    try {
+      res.status(200);
+      res.json(dbAccess.getConsumers());
+    } catch (err) {
+      // Internal error on the server side.
+      console.log(err);
+      res.status(500);
+      res.json(err);
+    }
+    return res;
+  });
 
-	// Retrieve all bids
-	app.get("/api/bid", function (req, res) {
-		try {
-			res.status(200);
-			res.json(bids.getRecords());
-		}
-		catch (err) {
-			// Internal error on the server side.
-			console.log(err);
-			res.status(500);
-			res.send(err);
-		}
-		return res;
-	});
+  // Retrieve all bids
+  app.get('/api/bid', function (req, res) {
+    try {
+      res.status(200);
+      res.json(dbAccess.getBids());
+    } catch (err) {
+      // Internal error on the server side.
+      console.log(err);
+      res.status(500);
+      res.send(err);
+    }
+    return res;
+  });
 
-	// Retrieve all listings
-	app.get("/api/listing", function (req, res) {
-		try {
-			res.status(200);
-			res.json(listings.getRecords());
-		}
-		catch (err) {
-			// Internal error on the server side.
-			console.log(err);
-			res.status(500);
-			res.send(err);
-		}
-		return res;
-	});
+  // Retrieve all listings
+  app.get('/api/listing', function (req, res) {
+    try {
+	  console.log('/api/listing');
+      res.status(200);
+      res.json(dbAccess.getListings());
+    } catch (err) {
+      // Internal error on the server side.
+      console.log(err);
+      res.status(500);
+      res.send(err);
+    }
+    return res;
+  });
 
-	// Delete an agent
-	app.delete("/api/agent/:id", function (req, res) {
-		try {
-			const success = agents.deleteRecord(req.params.id);
+  // Delete an agent
+  app.delete('/api/agent/:id', function (req, res) {
+    try {
+      const success = dbAccess.deleteAgent(req.params.id);
 
-			if (success) {
-				res.status(204); // HTML 204 request succeeded
-			} else {
-				res.status(404); // HTML status 404 not found
-			}
-			res.send(agents.getRecords());
-		}
-		catch (err) {
-			// Internal error on the server side.
-			console.log(err);
-			res.status(500);
-			res.send(err);
-		}
-		return res;
-	});
+      if (success) {
+        res.status(204); // HTML 204 request succeeded
+      } else {
+        res.status(404); // HTML status 404 not found
+      }
+      res.send(dbAccess.getAgents());
+    } catch (err) {
+      // Internal error on the server side.
+      console.log(err);
+      res.status(500);
+      res.send(err);
+    }
+    return res;
+  });
 
-	// Delete a consumer
-	app.delete("/api/consumer/:id", function (req, res) {
-		try {
-			const success = consumer.deleteRecord(req.params.id);
+  // Delete a consumer
+  app.delete('/api/consumer/:id', function (req, res) {
+    try {
+      const success = dbAccess.deleteConsumer(req.params.id);
 
-			if (success) {
-				res.status(204); // HTML 204 request succeeded
-			} else {
-				res.status(404); // HTML status 404 not found
-			}
-			res.send(consumers.getRecords());
-		}
-		catch (err) {
-			// Internal error on the server side.
-			console.log(err);
-			res.status(500);
-			res.send(err);
-		}
-		return res;
-	});
+      if (success) {
+        res.status(204); // HTML 204 request succeeded
+      } else {
+        res.status(404); // HTML status 404 not found
+      }
+      res.send(dbAccess.getConsumers());
+    } catch (err) {
+      // Internal error on the server side.
+      console.log(err);
+      res.status(500);
+      res.send(err);
+    }
+    return res;
+  });
 
-	// Delete a bid
-	app.delete("/api/bid/:id", function (req, res) {
-		try {
-			const success = bids.deleteRecord(req.params.id);
+  // Delete a bid
+  app.delete('/api/bid/:id', function (req, res) {
+    try {
+      const success = dbAccess.deleteBid(req.params.id);
 
-			if (success) {
-				res.status(204); // HTML 204 request succeeded
-			} else {
-				res.status(404); // HTML status 404 not found
-			}
-			res.send(bids.getRecords());
-		}
-		catch (err) {
-			// Internal error on the server side.
-			console.log(err);
-			res.status(500);
-			res.send(err);
-		}
-		return res;
-	});
+      if (success) {
+        res.status(204); // HTML 204 request succeeded
+      } else {
+        res.status(404); // HTML status 404 not found
+      }
+      res.send(dbAccess.getBids());
+    } catch (err) {
+      // Internal error on the server side.
+      console.log(err);
+      res.status(500);
+      res.send(err);
+    }
+    return res;
+  });
 
-	app.delete("/api/listing/:id", function (req, res) {
-		try {
-			const success = listings.deleteRecord(req.params.id);
+  app.delete('/api/listing/:id', function (req, res) {
+    try {
+      const success = dbAccess.deleteListing(req.params.id);
 
-			if (success) {
-				res.status(204); // HTML 204 request succeeded
-			} else {
-				res.status(404); // HTML status 404 not found
-			}
-			res.send(listings.getRecords());
-		}
-		catch (err) {
-			// Internal error on the server side.
-			console.log(err);
-			res.status(500);
-			res.send(err);
-		}
-		return res;
-	});
+      if (success) {
+        res.status(204); // HTML 204 request succeeded
+      } else {
+        res.status(404); // HTML status 404 not found
+      }
+      res.send(dbAccess.getListings());
+    } catch (err) {
+      // Internal error on the server side.
+      console.log(err);
+      res.status(500);
+      res.send(err);
+    }
+    return res;
+  });
 
-	// Update an agent
-	app.put("/api/agent/:id", function (req, res) {
-		try {
-			const success = agents.updateRecord(req.params.id, req.body);
+  // Update an agent
+  app.put('/api/agent/:id', function (req, res) {
+    try {
+      const success = dbAccess.updateAgent(req.params.id, req.body);
 
-			if (success) {
-				res.status(204); // HTML 204 request succeeded
-			} else {
-				res.status(404); // HTML status 404 not found
-			}
-			res.send(agents.getRecords());
-		}
-		catch (err) {
-			// Internal error on the server side.
-			console.log(err);
-			res.status(500);
-			res.send(err);
-		}
-		return res;
-	});
+      if (success) {
+        res.status(204); // HTML 204 request succeeded
+      } else {
+        res.status(404); // HTML status 404 not found
+      }
+      res.send(dbAccess.getAgents());
+    } catch (err) {
+      // Internal error on the server side.
+      console.log(err);
+      res.status(500);
+      res.send(err);
+    }
+    return res;
+  });
 
-	// Update a consumer
-	app.put("/api/consumer/:id", function (req, res) {
-		try {
-			const success = consumers.updateRecord(req.params.id, req.body);
+  // Update a consumer
+  app.put('/api/consumer/:id', function (req, res) {
+    try {
+      const success = dbAccess.updateConsumer(req.params.id, req.body);
 
-			if (success) {
-				res.status(204); // HTML 204 request succeeded
-			} else {
-				res.status(404); // HTML status 404 not found
-			}
-			res.send(consumers.getRecords());
-		}
-		catch (err) {
-			// Internal error on the server side.
-			console.log(err);
-			res.status(500);
-			res.send(err);
-		}
-		return res;
-	});
-
-	// Update a bid
-	app.put("/api/bid/:id", function (req, res) {
-		try {
-			const success = bids.updateRecord(req.params.id, req.body);
-
-			if (success) {
-				res.status(204); // HTML 204 request succeeded
-			} else {
-				res.status(404); // HTML status 404 not found
-			}
-			res.send(bids.getRecords());
-		}
-		catch (err) {
-			// Internal error on the server side.
-			console.log(err);
-			res.status(500);
-			res.send(err);
-		}
-		return res;
-	});
-
-	// Update a listing
-	app.put("/api/listing/:id", function (req, res) {
-		try {
-			const success = listings.updateRecord(req.params.id, req.body);
-
-			if (success) {
-				res.status(204); // HTML 204 request succeeded
-			} else {
-				res.status(404); // HTML status 404 not found
-			}
-			res.send(listings.getRecords());
-		}
-		catch (err) {
-			// Internal error on the server side.
-			console.log(err);
-			res.status(500);
-			res.send(err);
-		}
-		return res;
-	});
+      if (success) {
+        res.status(204); // HTML 204 request succeeded
+      } else {
+        res.status(404); // HTML status 404 not found
+      }
+      res.send(dbAccess.getConsumers());
+    } catch (err) {
+      // Internal error on the server side.
+      console.log(err);
+      res.status(500);
+      res.send(err);
+    }
+    return res;
+  });
 
 
-	// Create an agent
-	app.post("/api/agent", function (req, res) {
-		try {
-			res.status(201); // HTML status 201 creation successful
-			res.send(agents.addRecord(req.body));
-		}
-		catch (err) {
-			// Internal error on the server side.
-			console.log(err);
-			res.status(500);
-			res.send(err);
-		}
-		return res;
-	});
+  // Update a bid's status
+  app.put('/api/bid/:id/:status', function (req, res) {
+    try {
+      const success = dbAccess.updateBidStatus(req.params.id, req.params.status);
 
-	// Create a consumer
-	app.post("/api/consumer", function (req, res) {
-		try {
-			res.status(201); // HTML status 201 creation successful
-			res.send(consumers.addRecord(req.body));
-		}
-		catch (err) {
-			// Internal error on the server side.
-			console.log(err);
-			res.status(500);
-			res.send(err);
-		}
-		return res;
-	});
+      if (success) {
+        res.status(204); // HTML 204 request succeeded
+      } else {
+        res.status(404); // HTML status 404 not found
+      }
+      res.send(dbAccess.getBids(req.params.status));
+    } catch (err) {
+      // Internal error on the server side.
+      console.log(err);
+      res.status(500);
+      res.send(err);
+    }
+    return res;
+  });
 
-	// Create a bid
-	app.post("/api/bid", function (req, res) {
-		try {
-			res.status(201); // HTML status 201 creation successful
-			res.send(bids.addRecord(req.body));
-		}
-		catch (err) {
-			// Internal error on the server side.
-			console.log(err);
-			res.status(500);
-			res.send(err);
-		}
-		return res;
-	});
+  // Update a listings's status, not the entire listing
+  app.put('/api/listing/:id/:status', function (req, res) {
+    try {
+      const success = dbAccess.updateListingStatus(req.params.id, req.params.status);
 
-	// Create a listing
-	app.post("/api/listing", function (req, res) {
-		try {
-			res.status(201); // HTML status 201 creation successful
-			res.send(listings.addRecord(req.body));
-		}
-		catch (err) {
-			// Internal error on the server side.
-			console.log(err);
-			res.status(500);
-			res.send(err);
-		}
-		return res;
-	});
-}
+      if (success) {
+        res.status(204); // HTML 204 request succeeded
+      } else {
+        res.status(404); // HTML status 404 not found
+      }
+      res.send(dbAccess.getListings(req.params.status)); 
+    } catch (err) {
+      // Internal error on the server side.
+      console.log(err);
+      res.status(500);
+      res.send(err);
+    }
+    return res;
+  });
 
 
+  // Create an agent
+  app.post('/api/agent', function (req, res) {
+    try {
+      res.status(201); // HTML status 201 creation successful
+      res.send(dbAccess.createAgent(req.body));
+    } catch (err) {
+      // Internal error on the server side.
+      console.log(err);
+      res.status(500);
+      res.send(err);
+    }
+    return res;
+  });
+
+  // Create a consumer
+  app.post('/api/consumer', function (req, res) {
+    try {
+      res.status(201); // HTML status 201 creation successful
+      res.send(dbAccess.createConsumer(req.body));
+    } catch (err) {
+      // Internal error on the server side.
+      console.log(err);
+      res.status(500);
+      res.send(err);
+    }
+    return res;
+  });
+
+  // Create a bid
+  app.post('/api/bid', function (req, res) {
+    try {
+      res.status(201); // HTML status 201 creation successful
+      res.send(dbAccess.createBid(req.body));
+    } catch (err) {
+      // Internal error on the server side.
+      console.log(err);
+      res.status(500);
+      res.send(err);
+    }
+    return res;
+  });
+
+  // Create a listing
+  app.post('/api/listing', function (req, res) {
+    try {
+      res.status(201); // HTML status 201 creation successful
+      res.send(dbAccess.createListing(req.body));
+    } catch (err) {
+      // Internal error on the server side.
+      console.log(err);
+      res.status(500);
+      res.send(err);
+    }
+    return res;
+  });
+};
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Dependencies
 // =============================================================
-let express = require("express");
+let express = require('express');
+let DBAccess = require('./lib/dbAccess');
 
 // Sets up the Express App
 // =============================================================
@@ -10,16 +11,19 @@ let PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-//Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// Routes
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+  console.log('App listening on PORT ' + PORT);
 });
 
-
+// Close your database connection when Node exits
+process.on('exit', async function (code) {
+    return console.log(`About to exit with code ${code}`);
+});
