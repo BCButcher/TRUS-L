a3e0736535cb899169c274fae0328da8318d1446
ruthtech <leeruths@gmail.com>
2019-11-11 14:05:28 -0500

    pre merge commit on teams changes

--- public/assets/ profile/profile.js
+++ public/assets/ profile/profile.js
@@ -1,17 +1,19 @@
 // adding new listing
-function createListing() {
-  let listing = {
-    typeofHome: $("#typeOfHome").val(),
-    first_name: $("#address").val(),
-    last_name: $("#estimatedVal").val()
+function createListing(id) {
+  let newlisting = {
+    poster_id: id,
+    property_address: $('#address').val(),
+    listing_status:'Active',
+    estimated_value:$('#estimatedVal').val(),
+    transaction_type: 'abcd'
   };
 
   $.ajax({
-    url: "/api/listing/",
-    method: "POST",
+    url: '/api/listing/',
+    method: 'POST',
     data: newlisting
-  }).then(function(newListing) {
-    $("#listingConfirmation").appendTo(`
+  }).then(function (newListing) {
+    $('#listingConfirmation').append(`
   
             <h3> PROFILE CREATED</h3>
             <li>Display name: ${newListing.typeofHome}</li>
@@ -25,18 +27,18 @@ function createListing() {
 
 function createConsumer() {
   let consumer = {
-    display_name: $("#displayName").val(),
-    first_name: $("#firstName").val(),
-    last_name: $("#lastName").val(),
-    email: $("#email").val()
+    display_name: $('#displayName').val(),
+    first_name: $('#firstName').val(),
+    last_name: $('#lastName').val(),
+    email: $('#email').val()
   };
 
   // eslint-disable-next-line no-undef
   $.ajax({
-    url: "/api/consumer/",
-    method: "POST",
+    url: '/api/consumer/',
+    method: 'POST',
     data: consumer
-  }).then(function(newConsumer) {
+  }).then(function (newConsumer) {
     let confirmationMessage = `
                  PROFILE CREATED
                  Display name: ${newConsumer.displayName}
@@ -47,6 +49,6 @@ function createConsumer() {
               `;
     alert(confirmationMessage);
   });
-  createListing();
-  window.location.href = "/confirm.html";
+  createListing(newConsumer.id);
+  window.location.href = '/confirm.html';
 }
