69d4488317b55acdd720731f77ae43c2642abec1
ruthtech <leeruths@gmail.com>
2019-11-12 11:10:20 -0500

    Update files to be mobile first and extract styling to external file.

---
+++ public/assets/listings/client_criteria.js
@@ -0,0 +1,8 @@
+
+function updateSlider() {
+    var slider = document.getElementById("buyer_value_slider");
+    var output = document.getElementById("value");
+    output.innerHTML = buyer_value_slider.value; // Display the default slider value
+}
+
+
