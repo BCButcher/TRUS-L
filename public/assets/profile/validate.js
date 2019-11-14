f764bac73e31c6279752b6c35e5eb44188d9c050
Brian Butcher <butcher.brian.c@gmail.com>
2019-11-14 13:45:17 -0500

    added validate.js for profiles

---
+++ public/assets/profile/validate.js
@@ -0,0 +1,5 @@
+const isValidEmail = email_input_profileC.checkValidity();
+
+email_input_profilec.addEventListener('keyup', function(event) {
+    isValidEmail = email_input_profiec.checkValidity();
+});
