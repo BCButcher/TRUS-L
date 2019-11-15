b91ddc9912b788df63b1951ee3e6c756b3342498
ruthtech <leeruths@gmail.com>
2019-11-15 02:58:05 -0500

    Remove some unnecessary comments

--- public/assets/bids/counterbid.js
+++ public/assets/bids/counterbid.js
@@ -1,14 +1,6 @@
 function createCounterBid() {
     let storedService = getServices();
-    // const userInfo = getUserInfo();
     let listId = getParam()[1];
-    // let bidInfo = {
-    //     agent_id: userInfo.agent_id,
-    //     listing_id: listId,
-    //     bid_status: 'Active',
-    //     services: storedService,
-    //     message: $('#offer').val() 
-    // }
     // console.log(bidInfo);
     $.ajax({
         url: "/api/bid/active/" + listId + "/" + storedService + "/" + $('#offer').val(),
