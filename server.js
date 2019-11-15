530a15a3cd114f5ec3f304772d4bb16fd4ec6c80
ruthtech <leeruths@gmail.com>
2019-11-15 12:37:48 -0500

    PORT fix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ let path = require('path');
 // Sets up the Express App
 // =============================================================
 let app = express();
-let PORT = 3000;
+let PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
