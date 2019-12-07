02544dac38a9bafe826016acc77356d233c439cc
Brian Butcher <butcher.brian.c@gmail.com>
2019-12-07 18:36:24 -0500

    added 500 character limit counter to message window

--- public/assets/js/contact.js
+++ public/assets/js/contact.js
@@ -1,4 +1,13 @@
-$( document ).ready(async function() {
+$(document).ready(async function() {
     generateDisplayName();
     generateLoginLogout();
-  });
+});
+
+function countChar(val) {
+    var len = val.value.length;
+    if (len >= 500) {
+        val.value = val.value.substring(0, 500);
+    } else {
+        $('#charNum').text(500 - len);
+    }
+};
